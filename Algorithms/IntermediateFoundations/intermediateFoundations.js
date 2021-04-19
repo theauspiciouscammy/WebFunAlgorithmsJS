c9c8c9befef5737ece494c4c0d67bb9720c0af39
Cameron Smith <theauspiciouscammy@gmail.com>
2021-04-18 17:50:03 -0700

    4/18 commit

--- Algorithms/IntermediateFoundations/intermediateFoundations.js
+++ Algorithms/IntermediateFoundations/intermediateFoundations.js
@@ -1,94 +1,81 @@
-// function Sigma(num){
-//     var sum = 0
-//     for( var i = 0; i <= num; i++){
-//         sum += i
-//     }console.log(sum)
-// }
-// var num = 3
-// Sigma(num)
+'use strict';
+function Sigma(num){
+    var sum = 0;
+    for(var i = 0; i <= num; i++){
+        sum += i;
+    }
+    return sum;
+}
+var sigmaNum = 3
+//console.log(Sigma(sigmaNum));
 
-// function factorial(num){
-//     var fact = 1
-//     for( var i = 1; i <= num; i++){
-//         fact = fact * i
-//     }console.log(fact)
-// }
-// var num = 5
-// factorial(num)
+function factorial(num){
+    var fact = 1;
+    for(var i = 1; i <= num; i++){
+        fact *= i;
+    }
+    return fact;
+}
+var factNum = 3;
+//console.log(factorial(factNum));
 
-// function fibonacci(){
+function fibonacci(input){
+    var fib = [0,1]
+    for(var i = 0; i < fib[input]; i++){
 
-
-// }
-
-
-
-// fibonacci()
-
-// function second2Last(x){
-//     if(x.length < 2){
-//         return(null)
-//     }
-//     else{
-//         return(x[x.length-2])
-//     }
-// }
-
-// var x = [1,2,3,4,5,6]
-// console.log(second2Last(x))
-
-// function nth2Last(x, a){
-//     if(y = x[x.length-a]){
-//         // return(y);
-//         console.log(y);
-//     }
-//     else{
-//         // return(null)
-//         console.log(y);
-//     }
-// }
-
-// var x = [1,"Dojo",3,false,5,6]
-// console.log(nth2Last(x, 5)
-
-// function nth2Last(x, a){
-//     if(a < x.length){
-//         return(x[x.length - a])
-//     }
-//     else{
-//         return(null)
-//     }
-// }
-
-// var x = [1,"Dojo",3,false,5,6,4,5]
-// console.log(nth2Last(x, 5))
-
-// function secondGreatest(myArr){
-//     if(myArr.length < 2){
-//         return(null)
-//     }
-//     var max = myArr[0]
-//     var max2 = myArr[0]
-//     for( var i= 0; i < myArr.length; i++){
-//         if(myArr[i]> max){
-//             max2 = max
-//             max = myArr[i]
-//         }
-//         else if(myArr[i] > max2){
-//             max2 = myArr[i]
-//         }
-//     }return(max2)
-// }
-// var myArr = [67, 34, 2, 5, 16, 3, 4];
-// console.log(secondGreatest(myArr))
-
-function doubled(array){
-    var newarray = []
-    for(i = 0; i < array.length; i++){
-        newarray.push(array[i])
-        newarray.push(array[i])
-    }console.log(newarray)
+    }
 
 }
+var fibNum = 0;
+//console.log(fibonacci(fibNum));
+
+function second2Last(array){
+    if(array.length < 2){
+        return null;
+    }
+    return array[array.length-2];
+}
+var arrayS2L = [1,2,3,4,5,6];
+//console.log(second2Last(arrayS2L));
+
+function nth2Last(input, index){
+    if(index > input.length){
+        return null;
+    }
+    return input[input.length-index];
+
+}
+var myArray = [1,"Dojo",3,false,5,6];
+var index = 5;
+//console.log(nth2Last(myArray, index));
+
+function secondGreatest(arr){
+    if(arr.length < 2){
+        return null;
+    }
+    for(var i = 0; i < arr.length; i++){
+        var min = i;
+        for(var x = i+1; x < arr.length; x++){
+            if(arr[min] > arr[x]){
+                min = x;
+            }
+        }
+        var temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+    var almostMax = arr[arr.length-2];
+    return almostMax;
+}
+var myArr = [57,24,35,-3,2,46];
+//console.log(secondGreatest(myArr))
+
+function doubled(input){
+    var newArray = [];
+    for(var i = 0; i < input.length; i++){
+        newArray.push(input[i], input[i]);
+    }
+    return newArray;
+}
 var array = [4, "ulysses", 42, false]
-doubled(array)
+//console.log(doubled(array));
